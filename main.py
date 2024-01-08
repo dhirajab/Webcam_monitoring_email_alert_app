39b9eecb6154160444de2d98e0a0d5e55be75eb1
Dhiraj <dhirajborkar002@gmail.com>
2024-01-08 22:45:31 +0530

    Add gray frame

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ time.sleep(1)
 
 while True:
     check, frame = video.read()
-    cv2.imshow("My Video", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21,21),0)
+    cv2.imshow("My Video", gray_frame_gau)
 
     key = cv2.waitKey(1)
 
